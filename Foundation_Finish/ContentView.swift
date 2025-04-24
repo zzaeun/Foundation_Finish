dcaec9f20ca1c6113db03117ed1b4e880b167a95
zzaeun <leeje575@naver.com>
2025-04-24 15:30:47 +0900

    Initial Commit

---
+++ Foundation_Finish/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Foundation_Finish
+//
+//  Created by 이정은 on 4/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
